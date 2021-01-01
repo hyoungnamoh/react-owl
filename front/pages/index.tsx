53df4f72b4954cdd2012a50a22d78ac4f8a8c387
hyungnam <zzzsh789@naver.com>
2021-01-01 20:04:56 +0900

    Add: scss setting

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,13 +1,23 @@
 import { useCallback, useEffect, useRef, useState, FC } from "react";
 import { NextPage } from 'next'
 import React from 'react';
+import styles from '../styles/first.module.scss';
 
 interface Props {
-  userAgent?: string;
+  // userAgent?: string;
 }
-const Index: NextPage<Props> = ({ userAgent }) => (
-  <main>Your user agent: {userAgent}</main>
-)
+const Index: NextPage<Props> = ({ }) => {
+  return (
+    <button
+      type="button"
+      // Note how the "error" class is accessed as a property on the imported
+      // `styles` object.
+      className={styles.error}
+    >
+      Destroy
+    </button>
+  )
+}
 
 Index.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
