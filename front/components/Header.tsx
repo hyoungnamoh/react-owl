23e79db6518c4e04a3d98e68ee77fa313618e3e4
hyungnam <zzzsh789@naver.com>
2021-01-08 00:29:32 +0900

    Add: 로그인 시 헤더 변경

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -3,24 +3,58 @@ import { FC } from 'react';
 import styles from '../styles/header.module.scss';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { userStore } from '../store';
+import { useObserver } from 'mobx-react';
 const Header = () => {
   const router = useRouter()
+
   const moveLoginPage = (e: React.MouseEvent) => {
     e.preventDefault();
     router.push('/sign/logInPage');
   }
-  return (
+
+  const logOut = (e: React.MouseEvent) => {
+    e.preventDefault();
+    userStore.logOut();
+  }
+
+  const getLogo = (): string => {
+    return userStore.data ? '/images/owl_plz_beige.png' : '/images/owl_logo_real.png';
+  }
+
+  const getLogoStyle = (): React.CSSProperties => {
+    return userStore.data ? { width: 100, height: 35 } : {};
+  }
+  return useObserver(() => (
     <>
       <div className={styles.wrapper}>
         <Link href="/">
-          <img src='/images/owl_logo_real.png' />
+          <img src={getLogo()} style={getLogoStyle()} />
         </Link>
-        <button className={styles.moveLoginPageButton} onClick={moveLoginPage}>
-          <div className={styles.moveLoginPageButtonText}>Login / Register</div>
-        </button>
+        <div className={styles.rightButton}>
+          {
+            userStore.data ?
+              <>
+                <button className={styles.profilePicButton} style={{ marginRight: 20 }}>
+                  <img src="/images/profile.png" className={styles.profilePic} />
+                </button>
+                <button className={styles.profilePicButton} style={{ marginRight: 20 }}>
+                  <img src="/images/talk.png" className={styles.talk} />
+                </button>
+                <button className={styles.profilePicButton} onClick={logOut}>
+                  <img src="/images/noti.png" className={styles.noti} />
+                </button>
+              </>
+              :
+              <button className={styles.moveLoginPageButton} onClick={moveLoginPage}>
+                <p className={styles.moveLoginPageButtonText}>Login / Register</p>
+              </button>
+          }
+
+        </div>
       </div>
     </>
-  );
+  ));
 }
 
 export default Header;
--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -2,11 +2,11 @@ import { useObserver } from 'mobx-react';
 import * as React from 'react';
 import { } from 'react';
 import { userStore } from '../../store';
-
+import styles from '../../styles/home.module.scss';
 const Home = () => {
   console.log(userStore.data);
   return useObserver(() =>
-    <div>
+    <div className={styles.wrapper}>
       하이 홈
     </div>
   )
