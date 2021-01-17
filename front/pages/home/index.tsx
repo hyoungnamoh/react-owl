f3d2df3f7d6eb12de5d8567ca59dd85f91c7df98
hyungnam <zzzsh789@naver.com>
2021-01-17 21:51:12 +0900

    Modify: 홈 추가 및 수정, dayjs 추가

--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -3,15 +3,21 @@ import * as React from 'react';
 import { } from 'react';
 import { userStore } from '../../store';
 import styles from '../../styles/home.module.scss';
+import dayjs from 'dayjs'
 
-const homeData = null;
+interface HomeData {
+
+}
+const homeData: HomeData | null = null;
+const days: string[] = ['일', '월', '화', '수', '목', '금', '토'];
 const Home = () => {
-  const getWrappStyle = (): React.CSSProperties => {
-    return userStore.data ? { backgroundColor: '#fff' } : {}
-  }
+  const startDate: string = dayjs().startOf('weeks').add(1, 'day').format('YYYY-MM-DD') + '( 월 )';
+  const endDate: string = dayjs().endOf('weeks').add(1, 'day').format('YYYY-MM-DD') + '( 일 )';
+
+
   return useObserver(() =>
     <div className={styles.wrapper} >
-      <div className={styles.firstRow}>
+      <div className={styles.row}>
         <div className={styles.firstRowItem}>
           <div className={styles.title}>Issue Task</div>
           {
@@ -25,21 +31,32 @@ const Home = () => {
 
         </div>
         <div className={styles.firstRowItem} style={{ marginLeft: 70 }}>
-          <div className={styles.title}>Time Line</div>
+          <p className={styles.title}>Time Line</p>
+          <p className={styles.today}>Today: {dayjs().format('YYYY-MM-DD') + ' ' + '( ' + days[dayjs().day()] + ' )'}</p>
           {
             homeData ?
               <div></div>
               :
               <div className={styles.noAssignTextContainer}>
                 <p className={styles.noAssignText}>이번주 일정이 존재하지 않습니다.</p>
-                <p className={styles.noAssignText}>({})</p>
+                <p className={styles.noAssignText}>( {startDate} ~ {endDate} )</p>
               </div>
           }
         </div>
       </div>
-      <div>My Issue Progress Chart</div>
-      <div>Project Contribution Chart</div>
-      <div>Project Progress Chart</div>
+      <div className={styles.row} style={{marginTop: 50}} >
+        <div className={styles.myIssueChartContainer}>
+          <p className={styles.title}>My Issue Progress Chart</p>
+        </div>
+      </div>
+      <div className={styles.row} style={{marginTop: 50}} >
+        <div className={styles.firstRowItem}>
+          <p className={styles.title}>Project Contribution Chart</p>
+        </div>
+        <div className={styles.firstRowItem} style={{ marginLeft: 70 }}>
+          <p className={styles.title}>Preject Progress Chart</p>
+        </div>
+      </div>
     </div>
   )
 };
