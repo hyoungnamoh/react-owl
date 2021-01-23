de25b624ae3efde952d95a2785495b84b2965121
hyungnam <zzzsh789@naver.com>
2021-01-23 22:29:01 +0900

    Add: project setting menu

--- front/pages/project/projectList/projectDetail.tsx
+++ front/pages/project/projectList/projectDetail.tsx
@@ -30,10 +30,16 @@ const projectDetail = () => {
   const router = useRouter();
   const { id } = router.query;
   const [selected, setSelected] = useState<number>(0);
+  const [onProjectSet, setOnProjectSet] = useState<boolean>(true);
+
   const onClickTab = (index: number): void => {
-    console.log(index);
     setSelected(index);
   }
+
+  const onClickProjectSet = (): void => {
+    setOnProjectSet(prevOnProjectSet => !prevOnProjectSet);
+  }
+
   return useObserver(() =>
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -43,8 +49,22 @@ const projectDetail = () => {
         <div className={styles.tabContainer}>
           <Tab items={tabDumyData} selectedStyle={{ borderBottom: '#326295 3px solid' }} onClick={onClickTab} selected={selected} />
         </div>
-        <div className={styles.projectSettingButtonContainer}>
-          <FaUserCog color={'#326295'} style={{ width: '25%', height: '50%' }} />
+        <div className={styles.projectSettingButtonContainer} >
+          <FaUserCog color={'#326295'} style={{ width: '25%', height: '50%' }} onClick={() => onClickProjectSet()}/>
+          {
+            onProjectSet &&
+            <div className={styles.projectSettingMenu} >
+              <div className={styles.projectSettingMenuItem}>
+                <p className={styles.menuItemText}>프로젝트 멤버 추가</p>
+              </div>
+              <div className={styles.projectSettingMenuItem}>
+                <p>프로젝트 정보 확인</p>
+              </div>
+              <div className={styles.projectSettingMenuItem}>
+                <p>프로젝트 탈퇴</p>
+              </div>
+            </div>
+          }
         </div>
         {/* <img src={'/images/user-cog-solid.svg'} color={'#326295'}/> */}
       </div>
